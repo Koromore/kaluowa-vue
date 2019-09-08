9e14ac7066487a88ce149477637790a923638f3a
koromore <769745979@qq.com>
2019-09-08 22:47:37 +0800

    banner

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,15 +1,70 @@
 <template>
-  <div id="banner">
-    Banner
+  <div class="wrapper">
+    <swiper :options="swiperOption" v-if="showSwiper">
+      <!-- slides -->
+      <swiper-slide v-for="item of list" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" alt="">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Banner'
+  name: 'Banner',
+  // props: {
+  //   list: Array
+  // },
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        autoplay: 3000,
+        loop: true
+      },
+      list: [
+        {
+          imgUrl: require('../assets/images/banner/banner1.jpg')
+        },
+        {
+          imgUrl: require('../assets/images/banner/banner2.jpg')
+        },
+        {
+          imgUrl: require('../assets/images/banner/banner3.jpg')
+        },
+        {
+          imgUrl: require('../assets/images/banner/banner4.jpg')
+        },
+        {
+          imgUrl: require('../assets/images/banner/banner5.jpg')
+        },
+        {
+          imgUrl: require('../assets/images/banner/banner6.jpg')
+        }
+      ]
+    }
+  },
+  computed: {
+    showSwiper () {
+      return this.list.length
+    }
+  }
 }
 </script>
 
 <style scoped lang="stylus">
-
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    // height: 0
+    // padding-bottom: 31.25%
+    // background: #eee
+    .swiper-wrapper
+      width 100%
+      .swiper-img
+        width: 100%
 </style>
--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import './assets/styles/reset.css'
 import './assets/styles/border.css'
 
+Vue.use(VueAwesomeSwiper)
 Vue.config.productionTip = false
 fastClick.attach(document.body)
 
